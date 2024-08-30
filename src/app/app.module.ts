94102b9d0da17019510c88a771415f867fff301d
macpro <mmazhar@gmail.com>
2024-08-30 22:24:07 +0300

    completed home-screen but not finalized yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './features/home/presentation/home-page/home.component';
+import { HomePageComponent } from './features/home/presentation/home-page/home-page.component';
+import { SearchBarComponent } from './common/components/search-bar/search-bar.component';
+import { UserInfoComponent } from './features/home/presentation/home-page/components/user-info/user-info.component';
+import { CategoriesComponent } from './features/home/presentation/home-page/components/categories/categories.component';
+import { MainProductsCarouselComponent } from './features/home/presentation/home-page/components/main-products-carousel/main-products-carousel.component';
+import { FooterComponent } from './common/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomePageComponent,
+    SearchBarComponent,
+    UserInfoComponent,
+    CategoriesComponent,
+    MainProductsCarouselComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
