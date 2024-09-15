506bca96c6b45261e2502fbef973335ec23a8729
macpro <mmazhar@gmail.com>
2024-09-15 18:48:36 +0300

    changed home navigation click

--- src/app/common/components/header/header.component.ts
+++ src/app/common/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,10 @@ import {TranslateService} from "@ngx-translate/core";
 export class HeaderComponent implements OnInit {
 
   currentLanguage = "en"
-  constructor(private translate: TranslateService) { }
+  constructor(
+    private translate: TranslateService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.translate.onLangChange.subscribe((event) => {
@@ -20,6 +24,11 @@ export class HeaderComponent implements OnInit {
   changeLanguage(language: string) {
     console.log("Change language to ", language)
     this.translate.use(language)
+  }
+
+  goToHome() {
+    console.log("Go to home page")
+    this.router.navigate(['/']).then()
   }
 
 }
