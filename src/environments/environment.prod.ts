ec675de004498c28ed4678deb3eb0ea23b418dbe
macpro <mmazhar@gmail.com>
2025-01-05 17:03:04 +0200

    redirected to etoileeg.online

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,5 @@ export const environment = {
   payMobBaseUrl: 'https://accept.paymob.com/api',
   payMobPrivate: 'egy_sk_test_d3ea9b4c5082a6d01ffe0e1ed8010a66c36a8055a38939eb2fb8cae78845faad',
   payMobPublic: 'egy_pk_test_IPKrLfV3hP8KBwIa0HEUz4YNQcQZnUTE',
-  payMobCallBackUrl: 'https://etoileEgy.com',
+  payMobCallBackUrl: 'https://www.etoileeg.online',
 };
